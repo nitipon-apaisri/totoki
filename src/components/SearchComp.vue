ad58ebd036ce9999a364f55d24fd1803a6a20ca7
codeninjah <codeninjahblog@gmail.com>
2021-02-09 16:02:26 +0100

    Vuex

--- src/components/SearchComp.vue
+++ src/components/SearchComp.vue
@@ -20,15 +20,57 @@ export default {
   },
   methods: {
     postInput() {
+
+      //Not working
+      /*
+      if(this.$root.getInput(this.input).length == 0){
+        this.input = "";
+        break;
+      }
+      */
+
       this.$root.getInput(this.input);
+
+      
+      
       this.input = "";
       this.$router.push("/Gallery").catch((err) => {
         err;
       });
     },
+
+    btnStuff() {
+
+      /*
+      if(this.$root.getInput(this.input).length == 0)
+      {
+        document.getElementsByTagName("button")[0].classList.add("cursor")
+      }
+      */
+
+     console.log("beforeCreate med btn!")
+    }
   },
+
+    beforeCreate(){
+      console.log("beforeCreate!")
+      //this.btnStuff()
+    }
 };
 </script>
 
 <style lang="scss" scoped>
+
+/*
+button:hover {
+  background-color: black;
+  display: none;
+}
+*/
+
+.cursor:hover {
+  cursor: crosshair;
+  background-color: red;
+}
+
 </style>
